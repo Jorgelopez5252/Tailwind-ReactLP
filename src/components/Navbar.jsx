e9d42b1fabdbb87756beee8efb9b7a5bd3a88bc0
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2026-03-09 23:12:34 -0700

    completed basic set up, last set up was broken

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../components/Navbar";
+
+export const Home = () => {
+    return (
+        <div>
+            <Navbar />
+
+            <main>
+
+            </main>
+        </div>
+    )
+}
