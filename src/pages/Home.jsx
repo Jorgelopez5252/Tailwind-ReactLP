60030e628878784e82155babffc45095a6b380bc
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2026-03-11 15:00:45 -0700

    saving before i reboot machine, need to find out why videos are not autoplaying

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { Navbar } from "../components/Navbar";
-import {HeroSection} from "../components/HeroSection";
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
   return (
     <div>
       <Navbar />
-      <HeroSection />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
       <main></main>
     </div>
   );
