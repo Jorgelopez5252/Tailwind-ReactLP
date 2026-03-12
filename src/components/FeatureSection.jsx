5cc2985a7c9fafb6a485c22d65dc33beeca6a82d
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2026-03-12 13:32:15 -0700

    updated href tags to not

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -3,10 +3,10 @@ import { features } from "../constants";
 // React functional component
 const FeatureSection = () => {
   return (
-    <div className="relative mt-20 border-b border-neutral-800 min-h-200">
+    <div id="feature" className="relative mt-20 border-b border-neutral-800 min-h-200">
       {/* Section header container */}
       <div className="text-center">
-        <span className="bg-neutral-900 text-orange-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
+        <span href="feature" className="bg-neutral-900 text-orange-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
           features
         </span>
 
--- src/components/WorkFlowSection.jsx
+++ src/components/WorkFlowSection.jsx
@@ -4,7 +4,7 @@ import { checklistItems } from "../constants";
 
 const Workflow = () => {
   return (
-    <div className="mt-20">
+    <div id="workflow" className="mt-20">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
         Accelerate your{" "}
         <span className="bg-linear-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
